9b6c7ea418ff874dae27f9a1324c179caaabcbd3
lucydeveloper <ludmilla.girina@gmail.com>
2024-09-11 22:48:17 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# module_1_4.py
+my_string = input("Название: ")
+print("Количество символов:", len(my_string))
+print("Urban", my_string .upper())
+print("Urban", my_string .lower())
+print("Urban", my_string .replace(" ", ""))
+print("Urban", my_string[0])
+print("Urban", my_string[-1])
